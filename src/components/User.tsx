daa3e2c54d1be9fbebdf2320629843057848df0f
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-29 13:55:44 +0600

    fetch some data with useEffect and load data ui

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,17 +1,27 @@
 import React, { FC } from 'react';
+import UserModel from '../types/models';
 
-interface user {
-    name: string,
-    age: number,
-    addUser: () => void
+// interface Props {
+//     name: string,
+//     age: number,
+//     addUser: () => void
+// }
+
+interface Props {
+    user: UserModel;
+    addUser: (user: UserModel) => void
 }
 
-const User: FC<user> = ({ name, age, addUser }) => {
+const User: FC<Props> = ({ user, addUser }) => {
 
     return (
         <div>
-            <h1>Mr: {name} with {age} month experience in Web Development Site.</h1>
-            <button onClick={addUser}>Add Me</button>
+            <div>
+                <h2>Name: {user.name}</h2>
+                <h4>Email: {user.email}</h4>
+                <h4>Phone: {user.phone}</h4>
+                <button onClick={() => addUser(user)}>Add Me</button>
+            </div>
         </div>
     );
 };
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import UserModel from '../types/models';
 import User from './User';
 
 const Users = () => {
-    const handleAddUser = (): void => {
+    const [users, setUsers] = useState<UserModel[]>([]);
+    const [team, setTeam] = useState<UserModel[]>([]);
 
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+
+    const handleAddUser = (user: UserModel): void => {
+        const newTeam = [...team, user]
+        setTeam(newTeam)
     }
 
     return (
         <div>
-            <h1>My User</h1>
-            <User name="Md Mehedi" age={6} addUser={handleAddUser}></User>
+            {/* <User name="Md Mehedi" age={6} addUser={handleAddUser}></User> */}
+            <h3>Team size: {team.length}</h3>
+            {
+                users.map(user => <User user={user} addUser={handleAddUser}></User>)
+            }
         </div>
     );
 };
